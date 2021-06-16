0e48bedebf58ae3efa1cce734b40cb31fd4f1e1f
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-16 18:44:09 +0300

    Done with ToDoInput

--- src/components/ToDoInput.tsx
+++ src/components/ToDoInput.tsx
@@ -1,27 +1,30 @@
-import { TextField } from "@material-ui/core";
-import React, { useState } from "react";
+import { TextField } from '@material-ui/core';
+import React, { useState } from 'react';
 
-export const ToDoInput = ({ handleTodoPost }:any ) => {
-    const [name, setName] = useState('');
-    
-    const pressEnter = async (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            await handleTodoPost(name);
-            setName('');
-        };
-    };
+interface Props {
+    handleTodoPost: (name: string) => Promise<void>
+}
+export const ToDoInput: React.FC<Props> = ({ handleTodoPost }) => {
+  const [name, setName] = useState('');
 
-    return (
-        <TextField
-            multiline={true}
-            label='ToDo'
-            fullWidth
-            variant='outlined'
-            margin='normal'
-            onChange={e => setName(e.target.value)}
-            onKeyDown={(e) => pressEnter(e)}
-            value={name}
-        />
-    );
+  const pressEnter = async (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      await handleTodoPost(name);
+      setName('');
+    }
+  };
+
+  return (
+    <TextField
+      multiline={true}
+      label='ToDo'
+      fullWidth
+      variant='outlined'
+      margin='normal'
+      onChange={(e) => setName(e.target.value)}
+      onKeyDown={(e) => pressEnter(e)}
+      value={name}
+    />
+  );
 };
